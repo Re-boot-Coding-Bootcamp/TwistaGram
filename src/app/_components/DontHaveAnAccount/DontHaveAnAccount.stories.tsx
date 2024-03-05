ae94317d26fe8dfd37fa78ad2b77f04540cc93c3
Muzappar Erkin <muzappar.erkin0122@gmail.com>
2024-03-05 12:38:12 -0500

    Resovled all the change request

--- src/app/_components/DontHaveAnAccount/DontHaveAnAccount.stories.tsx
+++ src/app/_components/DontHaveAnAccount/DontHaveAnAccount.stories.tsx
@@ -2,22 +2,18 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Box } from "@mui/material";
 import { DontHaveAnAccount } from "./DontHaveAnAccount";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Authentication/DontHaveAnAccount",
   component: DontHaveAnAccount,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
   decorators: [
     (Story) => (
       <Box width={"350px"}>
-        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        {}
         <Story />
       </Box>
     ),
@@ -26,8 +22,6 @@ const meta = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
   args: {},
 };
--- src/app/_components/DontHaveAnAccount/DontHaveAnAccount.tsx
+++ src/app/_components/DontHaveAnAccount/DontHaveAnAccount.tsx
@@ -1,12 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, alpha } from "@mui/material";
 
 const DontHaveAnAccount = (): JSX.Element => {
   return (
     <Box display="flex" flexDirection="row" gap={1}>
-      <Typography id="dont-have-an-account" color={"#00000066"}>
+      {/* TODO: Update #00000066 and #258BB6this color with theme later. */}
+      <Typography id="dont-have-an-account" color={alpha("#000000", 0.46)}>
         Don’t have an account?
       </Typography>
-      <Typography id="sign-up-link" color={"#258BB6"}>
+      <Typography id="sign-up-link" color={alpha("#258BB6", 0.86)}>
         Sign up.
       </Typography>
     </Box>
