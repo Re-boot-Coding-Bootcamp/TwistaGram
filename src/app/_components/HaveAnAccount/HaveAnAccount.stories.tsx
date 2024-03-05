f52640c9c0c6a7dfa4207b3a582669128cd65038
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-03-05 16:08:39 -0600

    all the comments has been resolved

--- src/app/_components/HaveAnAccount/HaveAnAccount.stories.tsx
+++ src/app/_components/HaveAnAccount/HaveAnAccount.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Box } from "@mui/material";
-import HaveAnAccount from './HaveAnAccount';
+import { HaveAnAccount } from "./HaveAnAccount";
 
 const meta: Meta = {
   title: "Authentication/HaveAnAccount",
@@ -15,12 +15,11 @@ const meta: Meta = {
       </Box>
     ),
   ],
-}satisfies Meta<typeof HaveAnAccount>;
+} satisfies Meta<typeof HaveAnAccount>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-  },
+  args: {},
 };
