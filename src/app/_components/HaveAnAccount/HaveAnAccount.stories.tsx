29234f08e3ac66c59850d60825fc490a0484daf8
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-03-05 15:36:10 -0600

    HaveAnAccount components has been created

---
+++ src/app/_components/HaveAnAccount/HaveAnAccount.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Box } from "@mui/material";
+import HaveAnAccount from './HaveAnAccount';
+
+const meta: Meta = {
+  title: "Authentication/HaveAnAccount",
+  component: HaveAnAccount,
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <Box>
+        <Story />
+      </Box>
+    ),
+  ],
+}satisfies Meta<typeof HaveAnAccount>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+  },
+};
