db12e32aba218f748fed1624f8098fa34fbb1d15
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-03-05 15:36:10 -0600

    HaveAnAccount components has been created

--- src/app/_components/HaveAnAccount/HaveAnAccount.tsx
+++ src/app/_components/HaveAnAccount/HaveAnAccount.tsx
@@ -2,23 +2,17 @@ import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-
-{
-  /*Using styled-components approach to apply styles to the Typography component*/
-}
+/*Using styled-components approach to apply styles to the Typography component*/
 const GrayText = styled(Typography)({
   color: "#B0B0B0", // Light gray color
   cursor: "pointer", // Change cursor to pointer to indicate it's clickable
   "&:hover": {
     color: "#9A9A9A", // Darker gray color on hover to indicate interactivity
   },
-  whiteSpace: 'nowrap', // Prevent text from wrapping
+  whiteSpace: "nowrap", // Prevent text from wrapping
 });
 
-{
-  /*Using styled-components approach to apply styles to the Button component*/
-}
-
+/*Using styled-components approach to apply styles to the Button component*/
 interface HaveAnAccountProps {
   onLogin: () => void;
   onTextClick: () => void; // Handler for when the text is clicked
@@ -33,7 +27,8 @@ const HaveAnAccount: React.FC<HaveAnAccountProps> = ({
       <GrayText onClick={onTextClick}>Already have an account?</GrayText>
       <Button variant="text" color="primary" onClick={onLogin}>
         Log in
-      </Button>    </Box>
+      </Button>
+    </Box>
   );
 };
 
