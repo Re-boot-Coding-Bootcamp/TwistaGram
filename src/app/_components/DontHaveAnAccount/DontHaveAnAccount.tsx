022435adf75ecc5b8acfc0c47f78db346fe34c99
Muzappar Erkin <muzappar.erkin0122@gmail.com>
2024-03-05 13:01:36 -0500

    Added a call back function and a sx

--- src/app/_components/DontHaveAnAccount/DontHaveAnAccount.tsx
+++ src/app/_components/DontHaveAnAccount/DontHaveAnAccount.tsx
@@ -1,13 +1,26 @@
 import { Box, Typography, alpha } from "@mui/material";
 
-const DontHaveAnAccount = (): JSX.Element => {
+// Define interface for props with callback function
+interface DontHaveAnAccountProps {
+  // Function to be called on Sign Up click
+  onSignUpClick: () => void;
+}
+
+const DontHaveAnAccount = ({
+  onSignUpClick,
+}: DontHaveAnAccountProps): JSX.Element => {
   return (
     <Box display="flex" flexDirection="row" gap={1}>
       {/* TODO: Update #00000066 and #258BB6this color with theme later. */}
       <Typography id="dont-have-an-account" color={alpha("#000000", 0.46)}>
         Don’t have an account?
       </Typography>
-      <Typography id="sign-up-link" color={alpha("#258BB6", 0.86)}>
+      <Typography
+        id="sign-up-link"
+        color={alpha("#258BB6", 0.86)}
+        onClick={onSignUpClick}
+        sx={{ cursor: "pointer" }}
+      >
         Sign up.
       </Typography>
     </Box>
