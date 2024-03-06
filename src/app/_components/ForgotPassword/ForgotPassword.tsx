05d1adb6bb8153b3879bcea3ec06561df1eef04a
Babur Dilshat <baburdilshat@gmail.com>
2024-03-05 16:05:15 -0800

    adding dev branch

---
+++ src/app/_components/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography, Button } from "@mui/material";
+import React from "react";
+
+const ForgotPassword = (): JSX.Element => {
+  return (
+    <Box display="flex" flexDirection="column">
+      <Typography mb={1} variant="h5" sx={{ fontWeight: "bold" }}>
+        Password reset
+      </Typography>
+      <Typography mb={5} sx={{ color: "text.secondary" }}>
+        Your password has been successfully reset. click confirm to set a new
+        password
+      </Typography>
+
+      <Button
+        variant="contained"
+        style={{ backgroundColor: "#648DDB" }}
+        id="confirm-reset-password"
+      >
+        Confirm
+      </Button>
+    </Box>
+  );
+};
+
+export default ForgotPassword;
