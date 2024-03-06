ba06a2017b892712ccbe22c77593aee3bcd9f235
Muradil Erkin <miller.muradil@gmail.com>
2024-03-05 21:26:28 -0500

    some more reg form, trying to setup mui theme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#258BB6",
+      light: "#37abd6",
+      dark: "#185a81",
+    },
+  },
+});
+
+export default theme;
