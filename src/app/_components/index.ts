619b96cde956de0c0507aa877f392bb18dfe59d0
Muradil Erkin <miller.muradil@gmail.com>
2024-03-08 14:44:49 -0500

    add index.ts files

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -2,3 +2,6 @@ export { Button } from "./Button";
 export * from "./Navigation";
 export { RegistrationForm } from "./RegistrationForm";
 export { DontHaveAnAccount } from "./DontHaveAnAccount";
+export { GoogleLoginButton } from "./GoogleLogin";
+export { HaveAnAccount } from "./HaveAnAccount";
+export { LoginForm } from "./LoginForm";
