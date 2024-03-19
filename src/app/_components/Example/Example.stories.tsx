5a02f125d9a2d0401dfde432341e132d8c5af793
Muradil Erkin <miller.muradil@gmail.com>
2024-03-18 21:42:51 -0400

    example component

---
+++ src/app/_components/Example/Example.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import React from "react";
+import { Example } from "./Example";
+
+const meta = {
+  title: "Components/Example",
+  component: Example,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [(Story) => <Story />],
+} satisfies Meta<typeof Example>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: "Sign In Form",
+    onSubmit: (email: string, password: string) => {
+      console.log("Logging in with email:", email, "and password:", password);
+    },
+  },
+};
