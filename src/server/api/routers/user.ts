f8b44831e566a266b1cd730192185bdcb6d64c34
Muradil Erkin <miller.muradil@gmail.com>
2024-03-20 17:44:37 -0400

    BE changes

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  // publicProcedure,
+} from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  getCurrentUser: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.db.user.findFirst({
+      where: { id: ctx.session.user.id },
+    });
+  }),
+  updateCurrentUser: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        username: z.string(),
+        bio: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.update({
+        where: { id: ctx.session.user.id },
+        data: input,
+      });
+    }),
+});
