d4958746b3bd59259bb2de5f4e3cf613f0a4638a
Muradil Erkin <miller.muradil@gmail.com>
2024-03-26 18:21:51 -0400

    backend changes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -21,15 +21,51 @@ export const postRouter = createTRPCRouter({
         orderBy: { createdAt: "desc" },
       });
     }),
-
-  create: protectedProcedure
-    .input(z.object({ content: z.string().min(1) }))
+  createPost: protectedProcedure
+    .input(z.object({ content: z.string(), image: z.string() }))
     .mutation(async ({ ctx, input }) => {
       return ctx.db.post.create({
         data: {
           content: input.content,
+          image: input.image,
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
+    }),
+  likePost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.like.create({
+        data: {
+          post: { connect: { id: input.postId } },
+          user: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+  commentOnPost: protectedProcedure
+    .input(z.object({ postId: z.string(), comment: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.comment.create({
+        data: {
+          comment: input.comment,
+          post: { connect: { id: input.postId } },
+          user: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+  deletePost: protectedProcedure
+    .input(z.object({ postId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const post = await ctx.db.post.findUnique({
+        where: { id: input.postId },
+        select: { id: true, createdBy: true },
+      });
+      if (!post) {
+        throw new Error("Post not found");
+      }
+      if (post.createdBy.id !== ctx.session.user.id) {
+        throw new Error("You don't have permission to delete this post");
+      }
+      return ctx.db.post.delete({ where: { id: input.postId } });
     }),
 });
