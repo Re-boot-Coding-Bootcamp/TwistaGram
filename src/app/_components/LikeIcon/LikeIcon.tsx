2e51b1dbf75ff4f6999f57e1d95e15c28c01b776
Jack Vu <jacknvu98@gmail.com>
2024-03-28 14:26:02 -0400

    im cooking

--- src/app/_components/LikeIcon/LikeIcon.tsx
+++ src/app/_components/LikeIcon/LikeIcon.tsx
@@ -3,7 +3,7 @@
 import { Box, IconButton, Tooltip, Typography } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "~/trpc/react";
 import { enqueueSnackbar } from "notistack";
 import type { User } from "@prisma/client";
@@ -16,29 +16,38 @@ interface Props {
 
 const LikeIcon = ({ user, post }: Props): JSX.Element => {
   const { mutateAsync } = api.post.likePost.useMutation();
-  const [liked, setLiked] = useState(false);
+  const [displayLikedNumber, setDisplayLikedNumber] = useState(0);
+
+  const userLikedPost = post.likes
+    .map((object) => object?.userId)
+    .includes(user.id);
+  console.log(userLikedPost);
+  
   const handelLike = async () => {
-    setLiked((prev) => !prev);
 
-    try {
-      await mutateAsync({ postId: post.id });
-
-      enqueueSnackbar("Post created.", { variant: "success" });
-    } catch (error) {
-      enqueueSnackbar("Failed to create post, please try again.", {
-        variant: "error",
-      });
+    if (!userLikedPost) {
+      try {
+        await mutateAsync({ postId: post.id });
+      } catch (error) {
+        enqueueSnackbar("Failed to like post, please try again.", {
+          variant: "error",
+        });
+      }
     }
   };
 
+  useEffect(() => {
+    setDisplayLikedNumber(post.likes.length);
+  }, [post]);
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
-      <Tooltip title={liked ? "Unlike" : "Like"} placement="bottom" arrow>
+      <Tooltip title={userLikedPost ? "Unlike" : "Like"} placement="bottom" arrow>
         <IconButton color="error" aria-label="delete" onClick={handelLike}>
-          {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          {userLikedPost ? <FavoriteIcon /> : <FavoriteBorderIcon />}
         </IconButton>
       </Tooltip>
-      <Typography color="error">{post.likes.length}</Typography>
+      <Typography color="error">{displayLikedNumber}</Typography>
     </Box>
   );
 };
