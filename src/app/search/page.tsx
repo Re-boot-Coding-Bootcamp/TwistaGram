287f966e36301540e57a74d1341412813965b387
Muradil Erkin <miller.muradil@gmail.com>
2024-04-02 01:19:24 -0400

    search api

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,54 @@
-export default async function Search() {
-  return <div>Search page</div>;
+"use client";
+
+import { TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { SearchType } from "~/constants";
+import { api } from "~/trpc/react";
+import { LoadingScreen } from "../_components";
+import { debounce } from "lodash";
+
+export default function Search() {
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  const debouncedSearch = debounce((input: string) => {
+    setDebouncedQuery(input);
+  }, 500);
+
+  useEffect(() => {
+    if (query.length > 0) {
+      debouncedSearch(query);
+    }
+    return () => debouncedSearch.cancel();
+  }, [debouncedSearch, query]);
+
+  const { data, isFetching } = api.search.search.useQuery(
+    {
+      type: SearchType.User,
+      query: debouncedQuery,
+    },
+    {
+      enabled: debouncedQuery.length > 0,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    }
+  );
+
+  if (isFetching) {
+    return <LoadingScreen />;
+  }
+
+  console.log("==> query", query);
+  console.log("==> debouncedQuery", debouncedQuery);
+  console.log("==> data", data);
+
+  return (
+    <>
+      <TextField
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for user or keywords..."
+      />
+    </>
+  );
 }
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { postRouter } from "~/server/api/routers/post";
 import { userRouter } from "~/server/api/routers/user";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { notificationRouter } from "./routers/notification";
+import { searchRouter } from "./routers/search";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   post: postRouter,
   user: userRouter,
   notification: notificationRouter,
+  search: searchRouter,
 });
 
 // export type definition of API
